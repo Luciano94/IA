4018e6d017a691b88b5b014d01496dcb8eeb08cf
Martín Monterrosa <mmonterrosa94@gmail.com>
2019-09-07 10:59:38 -0300

    Improve Node System

--- Assets/Scripts/NodeSystem/Editor/NodeCreatorGUI.cs
+++ Assets/Scripts/NodeSystem/Editor/NodeCreatorGUI.cs
@@ -7,16 +7,18 @@ using UnityEditor;
 public class NodeCreatorGUI : Editor {
    
     public override void OnInspectorGUI() {
-        DrawDefaultInspector();
+        GUILayout.BeginHorizontal();
         NodeCreator nodeCreator = (NodeCreator)target;
         GUIStyle style = new GUIStyle(GUI.skin.button);
         style.normal.textColor = new Color(0.8f, 0f, 0f, 1f);
-        if (GUILayout.Button("Bake Nodes")) {
-            nodeCreator.BakeNodes();
-        }
-        GUI.contentColor = Color.red;
         if (GUILayout.Button("Clear Nodes", style)) {
             nodeCreator.ClearNodes();
         }
+        style.normal.textColor = Color.white;
+        if (GUILayout.Button("Bake Nodes")) {
+            nodeCreator.BakeNodes();
+        }
+        GUILayout.EndHorizontal();
+        DrawDefaultInspector();
     }
 }
--- Assets/Scripts/NodeSystem/NodeCreator.cs
+++ Assets/Scripts/NodeSystem/NodeCreator.cs
@@ -2,50 +2,124 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Adjacent {
+    public int top = -1;
+    public int left = -1;
+    public int right = -1;
+    public int bottom = -1;
+}
+ 
+[System.Serializable]
+public class Node {
+    public Vector3 position;
+    public Adjacent adjacent;
+    public Node parent;
+    public bool occupied;
+    public int validAdjacents = 0;
+
+    public Node(Vector3 pos) {
+        position = pos;
+        occupied = false;
+        adjacent = new Adjacent();
+    }
+}
+
 public class NodeCreator : MonoBehaviour {
-    public GameObject nodePrefab;
     public Vector3 size;
     public float interval;
-    public LayerMask affectedLayers;
-    private GameObject[] nodes;
+    public LayerMask terrainLayers;
+    public LayerMask obstaclesLayers;
+    [HideInInspector][SerializeField] private Node[] nodes;
+    private int width;
+    private int height;
 
-    public void BakeNodes()  {
+    public void BakeNodes() { 
         ClearNodes();
-        nodes = new GameObject[(Mathf.FloorToInt(size.x/interval) + 1) * (Mathf.FloorToInt(size.z/interval) + 1)];
+        width = Mathf.FloorToInt(size.x/interval) + 1;
+        height = Mathf.FloorToInt(size.z/interval) + 1;
+        nodes = new Node[width * height];
         Vector3 currentPos = transform.position - size*0.5f;
-        currentPos.y = transform.position.y + size.y*0.5f;
+        currentPos.y = transform.position.y + size.y;
         int iNode = 0;
-        float maxZ = size.z*0.5f;
-        float maxX = size.x*0.5f;
-        RaycastHit raycastHit;
+        float maxZ = transform.position.z + size.z*0.5f;
+        float maxX = transform.position.x + size.x*0.5f;
+        int column = 0;
+        int row = 0;
         while (currentPos.z < maxZ) {
             while (currentPos.x < maxX) {
-                nodes[iNode] = Instantiate(nodePrefab, currentPos, Quaternion.identity, transform);
-                if (Physics.Raycast(nodes[iNode].transform.position, Vector3.down, out raycastHit, size.y, affectedLayers)) {
-                    Vector3 newPos = nodes[iNode].transform.position;
+                Vector3 newPos = currentPos;
+                RaycastHit raycastHit;
+                if (Physics.Raycast(newPos, Vector3.down, out raycastHit, size.y, terrainLayers)) {
                     newPos.y -= raycastHit.distance;
-                    nodes[iNode].transform.position = newPos;
+                } 
+
+                nodes[iNode] = new Node(newPos);
+                nodes[iNode].occupied = Physics.Raycast(newPos, Vector3.up, out raycastHit, 1f, obstaclesLayers);
+
+                if (column > 0) {
+                    if (!nodes[iNode].occupied && !nodes[iNode - 1].occupied) {
+                        nodes[iNode].adjacent.left = iNode - 1;
+                        nodes[iNode].validAdjacents++;
+                        nodes[iNode - 1].adjacent.right = iNode;
+                        nodes[iNode - 1].validAdjacents++;
+                    }
                 }
+
+                if (row > 0) {
+                    if (!nodes[iNode].occupied && !nodes[iNode - width].occupied) {
+                        nodes[iNode].adjacent.bottom = iNode - width;
+                        nodes[iNode].validAdjacents++;
+                        nodes[iNode - width].adjacent.top = iNode;
+                        nodes[iNode - width].validAdjacents++;
+                    }
+                }
+
                 currentPos.x += interval;
+                column++;
                 ++iNode;
             }
             currentPos.z += interval;
             currentPos.x = transform.position.x - size.x*0.5f;
+            row++;
+            column = 0;
         }
     }
-
     
     public void ClearNodes()  {
-        if (nodes != null) {
-            int nodesCount = nodes.Length;
-            for (int i = 0; i < nodesCount; ++i) {
-                DestroyImmediate(nodes[i]);
-            }
-        }
+        nodes = null;
+        CancelInvoke();
     }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireCube(transform.position, size);
+        CheckObstacles();
+        int length = nodes?.Length ?? 0;
+        for (int i = 0; i < length; ++i) {
+            Node node = nodes[i];
+            if (node.occupied) {
+                Gizmos.color = Color.red;
+            } else if (node.validAdjacents == 4) {
+                Gizmos.color = Color.white;
+            } else if (node.validAdjacents == 3) {
+                Gizmos.color = Color.cyan;
+            } else if (node.validAdjacents == 2) {
+                Gizmos.color = Color.green;
+            } else if (node.validAdjacents == 1) {
+                Gizmos.color = Color.yellow;
+            } else {
+                Gizmos.color = Color.black;
+            }
+            Gizmos.DrawWireSphere(nodes[i].position, 1f);
+        }
+    }
+
+    private void CheckObstacles() {
+        int length = nodes?.Length ?? 0;
+        for (int i = 0; i < length; ++i) {
+            Node node = nodes[i];
+            RaycastHit raycastHit;
+            node.occupied = Physics.Raycast(node.position, Vector3.up, out raycastHit, 1f, obstaclesLayers);
+        }
     }
 }
