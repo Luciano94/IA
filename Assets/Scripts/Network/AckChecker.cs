f53bf7621c7d3a4bf019274930e06a39a9f2cf09
Marchin <mmonterrosa94@gmail.com>
2019-10-28 20:13:29 -0300

    Add read function to ack

--- Assets/Scripts/Network/AckChecker.cs
+++ Assets/Scripts/Network/AckChecker.cs
@@ -6,8 +6,7 @@ public class AckChecker : MonoBehaviour {
     public uint prevAckBitmask = 0;
     List<ISerializePacket> pendingPackets;
 
-    void Write(uint packetId)
-    {
+    void Write(uint packetId) {
         int diff = (int)((long)packetId - (long)currAck);
         if (diff > 0) {
             prevAckBitmask = prevAckBitmask << diff;
@@ -16,5 +15,16 @@ public class AckChecker : MonoBehaviour {
             prevAckBitmask |=  1U << (-diff);
         }
         currAck = packetId;
+    }
+
+    bool Read(uint id) {
+        int diff = currAck - id;
+        if (diff == 0) {
+            return true;
+        } else if (diff < 0 || diff > 32) {
+            return false;
+        } else {
+            return (prevAckBitmask & (1 << diff)) != 0;
+        }
     }
 }
